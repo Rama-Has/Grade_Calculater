9a699029d6c89b64992e5dcd07f8a8d05dcdf107
rama hasiba <ramahaseeba@gmail.com>
2021-12-06 22:11:46 +0200

    first commit

---
+++ Rama'sExam.js
@@ -0,0 +1,72 @@
+"use strict";
+
+(function(){
+
+    window.addEventListener('load', initial);
+
+    let compute = document.getElementById("compute");
+    let clear = document.getElementById("clear");
+    let earned = document.getElementsByClassName("earned");
+    let realMark = document.getElementsByClassName("max");
+    let resultArea = document.getElementById("resultArea");
+    let curve  = document.getElementById("curve");
+    let earnTotal=0;
+    let maxTotal = 0 ;
+    let per = 0;
+
+    function initial(){
+        compute.addEventListener('click', percentage);
+        curve.addEventListener('click',curveByFive);
+        clear.addEventListener('click', clearAll);
+    }
+
+    function percentage(){
+        for( let i=0; i< earned.length; i++){
+           earnTotal = earnTotal + Number(earned[i].value);
+        }
+        
+        for( let i=0; i< realMark.length; i++){
+            maxTotal = maxTotal + Number(realMark[i].value);
+        }
+
+        per = (earnTotal/maxTotal)*100;
+        let resultDiv = document.createElement('div');
+        let re = document.createElement('p');
+        re.innerHTML = per;
+        if(per >= 60){
+            resultDiv.className = "pass";
+        }else{
+            resultDiv.className = "fail";  
+        }
+        resultDiv.appendChild(re);
+        resultArea.appendChild(resultDiv);
+        maxTotal = 0;
+        earnTotal = 0;
+    }
+    
+    function curveByFive(){
+        per = per +5;
+        let resultDiv = document.createElement('div');
+        let re = document.createElement('p');
+        re.innerHTML = per;
+        if(per >= 60){
+            resultDiv.className = "pass";
+        }else{
+            resultDiv.className = "fail";  
+        }
+        resultDiv.appendChild(re);
+        resultArea.appendChild(resultDiv);
+    }
+
+    function clearAll(){
+        let failTotal = document.getElementsByClassName('fail');
+        for(let i =0 ; i<failTotal.length;i++){
+            failTotal[i].style.visibility = "hidden";
+        }
+        
+        let passT = document.getElementsByClassName("pass");
+        for(let i =0 ; i<passT.length;i++){
+            passT[i].style.visibility = "hidden";
+        }
+    }
+})();
